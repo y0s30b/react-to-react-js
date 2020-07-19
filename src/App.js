fba4b2027c7c2214d072a38b55fecc1724f29193
y0s30b <y0s30b.k@gmail.com>
2020-07-19 22:29:17 +0900

    음식 사진 출력, Component 함수에서 <style> 안 되나?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
-function Food(props) {
-  return <h3>I love {props.fav} 😀</h3>
+function Food({fav, picture}) {
+  return <div>
+  <div>I love {fav} 😀</div>
+  <img width="160" src={picture} border="2" alt={fav}></img>
+  </div>
 }
+
+const preferredFood = [
+  {
+    name: "Sushi",
+    image: "https://images.unsplash.com/photo-1570780775848-bc1897788ce0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  }, 
+  {
+    name: "Katsu",
+    image: "https://images.unsplash.com/photo-1591814252471-068b545dff62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  {
+    name: "Yakiniku",
+    image: "https://images.unsplash.com/photo-1494566942107-a6e23c42d69e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+  },
+  {
+    name: "Cold Soba",
+    image: "https://images.unsplash.com/photo-1519984388953-d2406bc725e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80"
+  },
+  {
+    name: "Sashimi",
+    image: "https://images.unsplash.com/photo-1534256958597-7fe685cbd745?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <Food fav="Sushi" />
-      <Food fav="Gyukatsu" />
-      <Food fav="Yakiniku" />
+      {preferredFood.map(food => (
+      <Food fav={food.name} picture={food.image} />
+      ))}
     </div>
   );
 }
